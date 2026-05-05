b6835e2179b1e4c59f7815707fe61fbe2fbfa633
lehongquan <quanle81.computer@gmail.com>
2026-05-05 21:27:56 +0700

    hoc react qhle81

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          lhqcomputer
+          Hoc React qhle81
         </a>
       </header>
     </div>
